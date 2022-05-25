b6d9cf141605e5e5f3a1b4d1883fb24ed1193a2f
Alex Ruan <alexruan2222@gmail.com>
2022-05-25 01:18:35 -0400

    added firebase/finished base clicker

--- BasicClickerApp/App.js
+++ BasicClickerApp/App.js
@@ -1,22 +1,83 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
-  View,
+  TextInput,
   TouchableOpacity,
 } from 'react-native';
 
+import firestore from '@react-native-firebase/firestore';
+
 const App = () => {
+  const [username, setUsername] = useState('base');
   const [score, setScore] = useState(0);
+  const [allScores, setAllScores] = useState([]);
+
+  //This useEffect runs once when this component is first rendered
+  useEffect(() => {
+    //fetch all scores, return ordered by score in descending order
+    getCurrentScores();
+  }, []);
+
+  //Get current leaderboard
+  const getCurrentScores = () => {
+    firestore()
+      .collection('scores')
+      .orderBy('score', 'desc')
+      .get()
+      .then(querySnapshot => {
+        const docs = [];
+        querySnapshot.forEach(doc => {
+          docs.push(doc.data());
+        });
+        setAllScores(docs);
+      })
+      .catch(error => {
+        console.log('Error getting documents: ', error);
+      });
+  };
+
+  //update user score on firebase
+  const updateUserScore = newScore => {
+    firestore()
+      .collection('scores')
+      .doc(username)
+      .set({username: username, score: score})
+      .then(() => {
+        //refetch current leaderboard
+        getCurrentScores();
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.userNameInput}
+        placeholder="Enter username"
+        onChangeText={setUsername}
+      />
       <TouchableOpacity
+        style={styles.button}
         onPress={() => {
           setScore(score + 1);
+          updateUserScore(score + 1);
         }}>
         <Text>Press me! Current score: {score}</Text>
       </TouchableOpacity>
+      <Text>Leadeboard</Text>
+      {allScores.map(userScore => {
+        return (
+          <>
+            <Text>
+              {userScore.username}: {userScore.score}
+            </Text>
+          </>
+        );
+      })}
     </SafeAreaView>
   );
 };
@@ -26,7 +87,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: 'lightblue',
   },
 });
 
